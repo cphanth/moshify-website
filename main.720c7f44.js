fd7e7d72ad73c9d804e8f00088619dd4a827b41b
Christina <cphanth@gmail.com>
2021-03-04 09:42:02 -0500

    removed minified versions of all files

--- main.720c7f44.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".collapsible").forEach((function(e){return e.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))}));
-//# sourceMappingURL=main.720c7f44.js.map
